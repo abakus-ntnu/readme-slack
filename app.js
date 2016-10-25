ad5f41b45d08206dcd9fea690d59eab3f7fc10e0
Mats Byrkjeland <matsbyr@gmail.com>
2016-10-25 12:48:25 +0200

    Add vinstraffer-backup

--- app.js
+++ app.js
@@ -9,6 +9,58 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const fetchVinstraffer = (req, res, pageId) => {
+  request({
+    url: `https://wiki.abakus.no/rest/api/content/${pageId}`,
+    headers: {
+      Authorization: `Basic ${base64.encode(`${credentials.username}:${credentials.password}`)}`,
+    },
+    qs: {
+      expand: 'body.storage',
+    },
+  },
+  (err, response, body) => {
+    if (err || response.statusCode !== 200) {
+      res.json({ text: 'Error fetching info from Confluence' });
+      return;
+    }
+
+    const parsedBody = JSON.parse(body).body.storage.value;
+    const listStartString = '<![CDATA[||Completed||Priority||Locked||CreatedDate||CompletedDate||Assignee||Name||';
+    const listStart = parsedBody.indexOf(listStartString) + `${listStartString}\n`.length;
+    const listEnd = parsedBody.indexOf(']]></ac:plain-text-body></ac:structured-macro>');
+
+    const rgx = /^.*(\|*){6}\|([^\s]+)(.*)\|$/;
+
+    const attachments = parsedBody.slice(listStart, listEnd)
+    .split('\n')
+    .filter(line =>
+      !req.body.text
+      || rgx.exec(line) === null
+      || rgx.exec(line)[2].toLowerCase() === req.body.text.toLowerCase())
+      .map((line) => {
+        const regexResult = rgx.exec(line);
+        return {
+          text: regexResult ? `${rgx.exec(line)[2]}${rgx.exec(line)[3]}` : '',
+        };
+      });
+
+    let text = 'Her er alle vinstraffene';
+    if (req.body.text && attachments.length === 0) {
+      text = `${req.body.text} har ingen vinstraffer.`;
+    } else if (req.body.text) {
+      text = `Her er ${req.body.text} sine vinstraffer`;
+    }
+
+    res.json({
+      response_type: 'in_channel',
+      text,
+      attachments,
+    });
+  });
+};
+
+
 app.post('/', (req, res) => {
   const url = `http://arkiv.abakus.no/${req.body.text.replace(' ', '+')}`;
   res.json({
@@ -18,55 +70,15 @@ app.post('/', (req, res) => {
 });
 
 app.post('/vinstraffer', (req, res) => {
-  request({
-    url: 'https://wiki.abakus.no/rest/api/content/5505145',
-    headers: {
-      Authorization: `Basic ${base64.encode(`${credentials.username}:${credentials.password}`)}`,
-    },
-    qs: {
-      expand: 'body.storage',
-    },
-  },
-    (err, response, body) => {
-      if (err || response.statusCode !== 200) {
-        res.json({ text: 'Error fetching info from Confluence' });
-        return;
-      }
+  fetchVinstraffer(req, res, '5505145');
+});
 
-      const parsedBody = JSON.parse(body).body.storage.value;
-      const listStart = parsedBody.indexOf('<![CDATA[||Completed||Priority||Locked||CreatedDate||CompletedDate||Assignee||Name||') + '<![CDATA[||Completed||Priority||Locked||CreatedDate||CompletedDate||Assignee||Name||\n'.length;
-      const listEnd = parsedBody.indexOf(']]></ac:plain-text-body></ac:structured-macro>');
-
-      const rgx = /^.*(\|*){6}\|([^\s]+)(.*)\|$/;
-
-      const attachments = parsedBody.slice(listStart, listEnd)
-        .split('\n')
-        .filter(line =>
-          !req.body.text
-          || rgx.exec(line) === null
-          || rgx.exec(line)[2].toLowerCase() === req.body.text.toLowerCase())
-        .map((line) => {
-          const regexResult = rgx.exec(line);
-          return {
-            text: regexResult ? `${rgx.exec(line)[2]}${rgx.exec(line)[3]}` : '',
-          };
-        });
-
-      let text = 'Her er alle vinstraffene';
-      if (req.body.text && attachments.length === 0) {
-        text = `${req.body.text} har ingen vinstraffer.`;
-      } else if (req.body.text) {
-        text = `Her er ${req.body.text} sine vinstraffer`;
-      }
-
-      res.json({
-        response_type: 'in_channel',
-        text,
-        attachments,
-      });
-    });
+app.post('/vinstraffer-backup', (req, res) => {
+  fetchVinstraffer(req, res, '6357255');
 });
 
 const server = app.listen(7363, () => {
   console.log('Listening on port %s...', server.address().port);
 });
+
+export default app;
--- test/vinstraffer.test.js
+++ test/vinstraffer.test.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const should = require('should');
 const assert = require('assert');
 const request = require('supertest');
@@ -5,9 +6,9 @@ const request = require('supertest');
 describe('Routing', () => {
   const url = 'http://localhost:7363';
 
-  const requestAndExpectAll = (text, done) => {
+  const requestAndExpectAll = (endpoint, text, done) => {
     request(url)
-    .post('/vinstraffer')
+    .post(`${endpoint[0] === '/' ? '' : '/'}${endpoint}`)
     .send({ text })
     // end handles the response
     .end((err, res) => {
@@ -21,13 +22,13 @@ describe('Routing', () => {
     });
   };
 
-  describe('Vinstraffer', () => {
+  describe('Vinstraffer readme', () => {
     it('should return all vinstraffer if text is empty string', (done) => {
-      requestAndExpectAll('', done);
+      requestAndExpectAll('vinstraffer', '', done);
     });
 
     it('should return all vinstraffer if text is null', (done) => {
-      requestAndExpectAll(null, done);
+      requestAndExpectAll('vinstraffer', null, done);
     });
 
     it('should return user\'s vinstraffer if text is username', (done) => {
@@ -44,6 +45,34 @@ describe('Routing', () => {
             // this is should.js syntax, very clear
           res.should.have.property('status', 200);
           res.body.text.should.equal('Her er mats sine vinstraffer');
+          done();
+        });
+    });
+  });
+
+  describe('Vinstraffer backup', () => {
+    it('should return all vinstraffer if text is empty string', (done) => {
+      requestAndExpectAll('vinstraffer-backup', '', done);
+    });
+
+    it('should return all vinstraffer if text is null', (done) => {
+      requestAndExpectAll('vinstraffer-backup', null, done);
+    });
+
+    it('should return user\'s vinstraffer if text is username', (done) => {
+      const text = 'larsen';
+
+      request(url)
+        .post('/vinstraffer-backup')
+        .send({ text })
+          // end handles the response
+        .end((err, res) => {
+          if (err) {
+            throw err;
+          }
+            // this is should.js syntax, very clear
+          res.should.have.property('status', 200);
+          res.body.text.should.equal('Her er larsen sine vinstraffer');
           done();
         });
     });
