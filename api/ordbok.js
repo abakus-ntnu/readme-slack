6c5c0674cd7b9c8564402a17baa956834d7bacf2
Mats Byrkjeland <matsbyr@gmail.com>
2017-02-14 23:08:14 +0100

    Crawl the dictionary to retrieve explanation

--- api/ordbok.js
+++ api/ordbok.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import cheerio from 'cheerio';
+import request from 'request';
 
 const router = new express.Router();
 
 router.post('/', (req, res) => {
   const url = `http://ordbok.uib.no/perl/ordbok.cgi?OPP=${req.body.text.replace(' ', '+')}`;
-  return res.json({
-    response_type: 'in_channel',
-    text: url,
+
+  request.get(url, (rq, rs) => {
+    const $ = cheerio.load(rs.body);
+
+    const t = $('.tydingC').first();
+    const l = t.children().map((i, el) => $(el).text()).get().join('');
+    return res.json({
+      response_type: 'in_channel',
+      text: t.text().replace(l, '').trim(),
+    });
   });
 });
 
